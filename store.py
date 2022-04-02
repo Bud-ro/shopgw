9912a2c7f3daa9c717a529fa63ba9b18cd8a2973
Carson <gamercarsonzero@gmail.com>
2022-04-02 19:12:44 -0400

    Refactored to use Listing class. Added type annotations

--- store.py
+++ store.py
@@ -1,94 +1,120 @@
 # Class for managing sqlite3 database
 import sqlite3
+from listing import Listing
+from typing import List, Union
+
 
 class Store:
     """
     Class for managing the listings database
     """
-    def __init__(self, dbfile):
+
+    NON_KEYS = "TITLE, PRICE, END_TIMESTAMP, IMAGE_URL, NOTIFIED"
+
+    def __init__(self, dbfile: str) -> None:
         self.dbfile = dbfile
         self.last_key = None
         self.create_table()
 
-    def create_table(self):
+    def create_table(self) -> None:
         with sqlite3.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "CREATE TABLE IF NOT EXISTS LISTING (ID INTEGER PRIMARY KEY, TITLE TEXT, PRICE INTEGER, END_TIMESTAMP INTEGER, IMAGE TEXT, NOTIFIED BOOL)"
+            query = (
+                "CREATE TABLE IF NOT EXISTS LISTING"
+                "(ID INTEGER PRIMARY KEY,"
+                " TITLE TEXT,"
+                " PRICE INTEGER,"
+                " END_TIMESTAMP INTEGER,"
+                " IMAGE_URL TEXT,"
+                " NOTIFIED BOOL)"
+            )
             cursor.execute(query)
             connection.commit()
-        pass
 
-    def add_listing(self, ID, title, price, end_timestamp, image, notified):
+    def add_listing(self, listing: Listing) -> bool:
         with sqlite3.connect(self.dbfile) as connection:
             cursor = connection.cursor()
 
             # Check to see if listing ID already exists
-            query = "SELECT ID, TITLE, PRICE, END_TIMESTAMP, IMAGE, NOTIFIED FROM LISTING WHERE (ID = ?)"
-            cursor.execute(query, (ID,))
+            query = f"SELECT ID, {self.NON_KEYS} FROM LISTING WHERE (ID = ?)"
+            cursor.execute(query, (listing.ID,))
             if not cursor.fetchone() == None:
                 connection.commit()
                 return False
-            
+
             # If it doesn't then insert it
-            query = "INSERT INTO LISTING (ID, TITLE, PRICE, END_TIMESTAMP, IMAGE, NOTIFIED) VALUES (?, ?, ?, ?, ?, ?)"
-            cursor.execute(query, (ID, title, price, end_timestamp, image, notified))
+            query = (
+                f"INSERT INTO LISTING (ID, {self.NON_KEYS}) VALUES (?, ?, ?, ?, ?, ?)"
+            )
+            cursor.execute(query, (*listing,))
             connection.commit()
             self.last_key = cursor.lastrowid
             return True
 
-    def delete_listing(self, ID):
+    def delete_listing(self, ID: Union[int, str]) -> None:
         with sqlite3.connect(self.dbfile) as connection:
             cursor = connection.cursor()
             query = "DELETE FROM LISTING WHERE (ID = ?)"
             cursor.execute(query, (ID,))
             connection.commit()
-    
-    def update_listing(self, ID, title, price, end_timestamp, image, notified):
+
+    def update_listing(self, listing: Listing) -> None:
         with sqlite3.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "UPDATE LISTING SET TITLE = ?, PRICE = ?, END_TIMESTAMP = ?, IMAGE = ?, NOTIFIED = ? WHERE (ID = ?)"
-            cursor.execute(query, (title, price, end_timestamp, image, notified, ID))
+            query = (
+                "UPDATE LISTING SET TITLE = ?, PRICE = ?, END_TIMESTAMP = ?,"
+                " IMAGE_URL = ?, NOTIFIED = ? WHERE (ID = ?)"
+            )
+            (_, *non_keys) = listing  # Tuple unpacking to exclude first (ID)
+            cursor.execute(query, (*non_keys, listing.ID))
             connection.commit()
 
-    def set_listing_notified(self, ID):
+    def get_listing(self, ID: Union[int, str]) -> Listing:
         with sqlite3.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "UPDATE LISTING SET NOTIFIED = TRUE WHERE (ID = ?)"
+            query = f"SELECT ID, {self.NON_KEYS} FROM LISTING WHERE (ID = ?)"
             cursor.execute(query, (ID,))
-            connection.commit()
+            result = cursor.fetchone()
+            if result is None:
+                raise KeyError("No listing with the ID exists")
+            listing = Listing(*result)
+        return listing
 
-    def get_listing(self, ID):
+    def get_listings(self) -> List[Listing]:
         with sqlite3.connect(self.dbfile) as connection:
             cursor = connection.cursor()
-            query = "SELECT ID, TITLE, PRICE, END_TIMESTAMP, IMAGE, NOTIFIED FROM LISTING WHERE (ID = ?)"
-            cursor.execute(query, (ID,))
-            return_tuple = tuple(cursor.fetchone())
-        return return_tuple
-
-    def get_listings(self):
-        with sqlite3.connect(self.dbfile) as connection:
-            cursor = connection.cursor()
-            query = "SELECT ID, TITLE, PRICE, END_TIMESTAMP, IMAGE, NOTIFIED FROM LISTING ORDER BY ID"
+            query = f"SELECT ID, {self.NON_KEYS} FROM LISTING ORDER BY ID"
             cursor.execute(query)
-            listings = [(ID, (title, price, end_timestamp, image, notified))
-                      for ID, title, price, end_timestamp, image, notified in cursor]
+            listings = [
+                Listing(ID, title, price, end_timestamp, image_url, notified)
+                for ID, title, price, end_timestamp, image_url, notified in cursor
+            ]
         return listings
 
-def db_test():
+
+def db_test() -> None:
     print("Testing Database")
     print("Making Store object...")
     my_store = Store("test.sqlite")
+
     print("Creating Table")
     my_store.create_table()
-    print("Adding Test Listing [102, 'Test Title 2', 421, 1648438552, 'https://testimage.url', False]")
-    result = my_store.add_listing(103, 'Test Title 3', 1000000, 1648438552, 'https://testimage.url', True)
-    if result:
-        print("Added row")
-    else:
-        print("Failed to add row")
+
+    test_listing = Listing(
+        104, "Test Title 4", 1337, 13123123, "https://testimage.url", True
+    )
+    print("Adding Test Listing: {test_listing}")
+
+    # Update listing
+    # my_store.update_listing(test_listing)
+    # Add Listing
+    # if my_store.add_listing(test_listing):
+    #     print("Added row")
+    # else:
+    #     print("Failed to add row")
     print("Retrieving all listings:")
     print(my_store.get_listings())
-    pass
+
 
 if __name__ == "__main__":
     db_test()
